8d068118316d0cb6072031a001aa840d655ebb1d
parthpatel <parthpatel5333@gamil.com>
2021-11-13 07:33:09 +0530

    updated database part

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/bookStore"
-    "URI": "mongodb+srv://Parth_15:P@rthp@tel15@dbserver.qp9c9.mongodb.net/survey?retryWrites=true&w=majority",
+    //"URI": "mongodb+srv://ashley:6x7TRG9OKI64hZW9@mongodbserver.ra4qo.mongodb.net/business_contacts?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://Parth_15:2o2mrAnabnSvWzIp@dbserver.czcz3.mongodb.net/survey?retryWrites=true&w=majority",
     "Secret": "SomeSecret"
 }
