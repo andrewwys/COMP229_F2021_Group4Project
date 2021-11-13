7c161495fa55c3ffb71b377c89ea9fe3c3476882
parthpatel <parthpatel5333@gamil.com>
2021-11-13 06:35:59 +0530

    added mongo atlas database uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/bookStore"
-    "URI": "mongodb+srv://ashley:6x7TRG9OKI64hZW9@mongodbserver.ra4qo.mongodb.net/business_contacts?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://Parth_15:P@rthp@tel15@dbserver.qp9c9.mongodb.net/survey?retryWrites=true&w=majority",
     "Secret": "SomeSecret"
 }
