a81be4e84fb9b9d1f64c38eace2897cf3c30c0a8
andrewwy <andrewsit.wy@gmail.com>
2021-12-06 15:30:31 -0500

    updated db schema

--- server/models/survey.js
+++ server/models/survey.js
@@ -7,7 +7,15 @@ let surveyModel = mongoose.Schema({
     description: String,
     createdDate: Date,
     editedDate: Date,
-    timesViewed: Number
+    timesViewed: int,
+	status: String,
+	questions: [{
+		questionTitle: String, 
+		type: String,      // YN, SHORT
+		responseY: int,    // number of Yes response
+		responseN: int,    // number of No response
+		responseText: [String]
+	}],
 },
 {
     collection: "update"
